964009f5438d3a4a23ac7699f9e25928129f8f3a
Alex Tiboldi <alex.tiboldi@ccsoftware.be>
2021-09-30 17:24:23 +0300

    first pages of amazona

---
+++ components/Layout.js
@@ -0,0 +1,41 @@
+import React from "react";
+import Head from "next/Head";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import useStyle from "../utils/styles";
+import NextLink from "next/link";
+import { Link } from "@mui/material";
+
+export default function Layout({ children }) {
+  const cssClass = useStyle();
+  return (
+    <div>
+      <Head>
+        <title>Amazona</title>
+      </Head>
+      <AppBar position="static" className={cssClass.navbar}>
+        <Toolbar>
+          <NextLink href="/" passHref>
+            <Link>
+              <Typography className={cssClass.brand}>amazona</Typography>
+            </Link>
+          </NextLink>
+          <div className={cssClass.grow}>
+            <NextLink href="/cart" passHref>
+              <Link>Cart</Link>
+            </NextLink>
+            <NextLink href="/login" passHref>
+              <Link>Login</Link>
+            </NextLink>
+          </div>
+        </Toolbar>
+      </AppBar>
+      <Container className={cssClass.main}>{children}</Container>
+      <footer className={cssClass.footer}>
+        <Typography>All rights reserved to Amazona</Typography>
+      </footer>
+    </div>
+  );
+}
